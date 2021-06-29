4fa5f5eb7231d258dea2143a88506a1afe7ef03d
John Wellbelove <fork@wellbelove.co.uk>
2021-06-29 10:28:33 +0100

    Added Arduimo examples

--- examples/Example_0_import_etl/Example_0_import_etl.ino
+++ examples/Example_0_import_etl/Example_0_import_etl.ino
@@ -1,10 +1,12 @@
-#include "Embedded_Template_Library.h"  // this is required for any more etl import when using Arduino IDE
+#include "Embedded_Template_Library.h"  // This is required for any more etl import when using Arduino IDE
 
-void setup(){
+void setup()
+{
   
 }
 
 
-void loop(){
+void loop()
+{
   
 }
