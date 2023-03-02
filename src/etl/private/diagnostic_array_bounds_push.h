235b8327e4d45b3d35927e5b05764b4a6627a731
John Wellbelove <jwellbelove@users.noreply.github.com>
2023-03-02 15:16:53 +0000

    Sync to 20.35.13

--- src/etl/private/diagnostic_array_bounds_push.h
+++ src/etl/private/diagnostic_array_bounds_push.h
@@ -7,7 +7,7 @@ Embedded Template Library.
 https://github.com/ETLCPP/etl
 https://www.etlcpp.com
 
-Copyright(c) 2021 John Wellbelove
+Copyright(c) 2023 John Wellbelove
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files(the "Software"), to deal
@@ -28,57 +28,17 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ******************************************************************************/
 
-#ifndef ETL_CRC_INCLUDED
-#define ETL_CRC_INCLUDED
+/*
+ * The header include guard has been intentionally omitted.
+ * This file is intended to evaluated multiple times by design.
+ */
 
-#include "crc8_ccitt.h"
-#include "crc8_cdma2000.h"
-#include "crc8_darc.h"
-#include "crc8_dvbs2.h"
-#include "crc8_ebu.h"
-#include "crc8_icode.h"
-#include "crc8_itu.h"
-#include "crc8_maxim.h"
-#include "crc8_rohc.h"
-#include "crc8_wcdma.h"
+#if defined(__GNUC__) && !defined(__clang__) && !defined(__llvm__)
+#pragma GCC diagnostic push 
+#pragma GCC diagnostic ignored "-Warray-bounds"
+#endif
 
-#include "crc16.h"
-#include "crc16_a.h"
-#include "crc16_arc.h"
-#include "crc16_aug_ccitt.h"
-#include "crc16_buypass.h"
-#include "crc16_ccitt.h"
-#include "crc16_cdma2000.h"
-#include "crc16_dds110.h"
-#include "crc16_dectr.h"
-#include "crc16_dectx.h"
-#include "crc16_dnp.h"
-#include "crc16_en13757.h"
-#include "crc16_genibus.h"
-#include "crc16_kermit.h"
-#include "crc16_maxim.h"
-#include "crc16_mcrf4xx.h"
-#include "crc16_modbus.h"
-#include "crc16_profibus.h"
-#include "crc16_riello.h"
-#include "crc16_t10dif.h"
-#include "crc16_teledisk.h"
-#include "crc16_tms37157.h"
-#include "crc16_usb.h"
-#include "crc16_x25.h"
-#include "crc16_xmodem.h"
-#include "crc16_m17.h"
-
-#include "crc32.h"
-#include "crc32_bzip2.h"
-#include "crc32_c.h"
-#include "crc32_d.h"
-#include "crc32_jamcrc.h"
-#include "crc32_mpeg2.h"
-#include "crc32_posix.h"
-#include "crc32_q.h"
-#include "crc32_xfer.h"
-
-#include "crc64_ecma.h"
-
+#if defined(__clang__) || defined(__llvm__)
+#pragma clang diagnostic push 
+#pragma clang diagnostic ignored "-Warray-bounds"
 #endif
